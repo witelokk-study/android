c30d0fadaaf5964be44ffdc0f3f8502d67de8daa
witelokk <witelokk@gmail.com>
2024-02-12 15:25:44 +0300

    implement prac1

---
+++ Prac1/app/src/main/java/com/witelokk/prac1/HelloActivity.java
@@ -0,0 +1,23 @@
+package com.witelokk.prac1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HelloActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_hello);
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+        TextView helloTextView = findViewById(R.id.hello_text_view);
+        helloTextView.setText(getString(R.string.hello, name));
+    }
+}
--- Prac1/app/src/main/java/com/witelokk/prac1/MainActivity.java
+++ Prac1/app/src/main/java/com/witelokk/prac1/MainActivity.java
@@ -1,6 +1,12 @@
 package com.witelokk.prac1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +25,31 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        EditText nameEditText = findViewById(R.id.name_edit_text);
+        Button nextButton = findViewById(R.id.next_button);
+
+        nameEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                nextButton.setEnabled(!s.toString().isEmpty());
+            }
+        });
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HelloActivity.class);
+                intent.putExtra("name", nameEditText.getText().toString());
+                startActivity(intent);
+            }
         });
     }
 }
